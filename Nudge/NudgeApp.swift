ca0d08cd71c4e4f63f6b6193af95bc06c4f6fff7
Benjamin <benjaminbelloeil@outlook.com>
2026-03-03 12:23:55 +0100

    Refactor app structure and implement onboarding flow; remove ContentView

--- Nudge/NudgeApp.swift
+++ Nudge/NudgeApp.swift
@@ -4,14 +4,38 @@
 //
 //  Created by Benjamin Belloeil on 3/3/26.
 //
-
 import SwiftUI
 
 @main
 struct NudgeApp: App {
+    @StateObject private var subscriptionManager = SubscriptionManager.shared
+
+    init() {
+        SubscriptionManager.shared.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppRootView()
+                .environmentObject(subscriptionManager)
         }
+    }
+}
+
+struct AppRootView: View {
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
+    @EnvironmentObject var subscriptionManager: SubscriptionManager
+
+    var body: some View {
+        Group {
+            if hasCompletedOnboarding {
+                ContentView()
+            } else {
+                OnboardingView(onComplete: {
+                    hasCompletedOnboarding = true
+                })
+            }
+        }
+        .animation(.easeInOut(duration: 0.4), value: hasCompletedOnboarding)
     }
 }
