03edb7a08c0f34e40325da0de4580ef51d5e469f
Benjamin <benjaminbelloeil@outlook.com>
2026-03-03 12:03:22 +0100

    Initial Commit

---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Benjamin Belloeil on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
